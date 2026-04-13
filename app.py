bad7a7c28fe5cf97f3cb1d2ef5194d0cdd2d59b2
Siddhesh-wagh <siddhesh.01092004@gmail.com>
2026-04-13 20:19:53 +0530

    connecting postgres

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# PostgreSQL connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gym_admin:gym123@localhost:5432/gym_saas'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 @app.route("/")
 def home():
-    return "Gym SaaS is running 🚀"
+    return jsonify({"message": "Connected to PostgreSQL successfully 🚀"})
 
 if __name__ == "__main__":
     app.run(debug=True)
