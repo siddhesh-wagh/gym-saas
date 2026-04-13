ee38185dc18bf8b5a33c8cc980d3486f3c557284
Siddhesh-wagh <siddhesh.01092004@gmail.com>
2026-04-13 20:22:24 +0530

    created tables, and granted access too admin

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
@@ -9,9 +10,51 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# -----------------------
+# Models (Tables)
+# -----------------------
+
+class Gym(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+
+class Plan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    duration_days = db.Column(db.Integer)
+
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    phone = db.Column(db.String(20))
+
+    join_date = db.Column(db.Date)
+    expiry_date = db.Column(db.Date)
+
+    gym_id = db.Column(db.Integer, db.ForeignKey('gym.id'))
+    plan_id = db.Column(db.Integer, db.ForeignKey('plan.id'))
+
+
+# -----------------------
+# Routes
+# -----------------------
+
 @app.route("/")
 def home():
-    return jsonify({"message": "Connected to PostgreSQL successfully 🚀"})
+    return jsonify({"message": "Tables ready 🚀"})
+
+
+# -----------------------
+# Create Tables
+# -----------------------
+
+with app.app_context():
+    db.create_all()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
